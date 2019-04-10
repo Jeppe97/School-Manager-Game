ce1325b7fe665d4daa4eab17216428534c70a686
Jeppe97 <44122288+Jeppe97@users.noreply.github.com>
2019-04-10 10:42:26 +0200

    Add files via upload

---
+++ HUD.java
@@ -0,0 +1,23 @@
+package real;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class HUD {
+	
+	private static int Mood = 1000;
+	
+	public void tick() {
+		Mood--;
+		
+		Mood = Game.clamp(Mood, 0, 100);
+	}
+	
+	public void render(Graphics g) {
+		g.setColor(Color.gray);
+		g.fillRect(15, 15, 200, 32);
+		g.setColor(Color.green);
+		g.fillRect(15, 15, Mood*2, 32);
+	}
+
+}

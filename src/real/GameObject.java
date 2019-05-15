79705e27e90a3735b9008d82059d013f3cb72de8
Jesper Öhman <44122288+Jeppe97@users.noreply.github.com>
2019-05-15 20:01:18 +0200

    Add files via upload

--- src/real/GameObject.java
+++ src/real/GameObject.java
@@ -4,6 +4,10 @@ import java.awt.Graphics;
 import java.io.IOException;
 
 public abstract class GameObject {
+	// money, mood and students etc are all GameObjects which extends this abstract class.
+	// This means that they all can use these variables that we create here and they also have to use the methods tick and render.
+	// Since we can create the variables here instead of in the money class for example it makes the code a lot less cluttered.
+	// And gives our game a nice structure since all the GameObjects classes are very similar, it's easy to navigate and also makes it very modular.
 	
 	protected int x;
 	protected int y;
